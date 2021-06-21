877072516f480b8ed5fe4fec11d2633ca66106b6
lincolnthedev <github@lincolnwilliams.com>
2021-06-21 12:39:04 -0400

    Init Commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install unattended-upgrades -y
+sudo dpkg-reconfigure -plow unattended-upgrades
+sudo apt install php openssh-server -y
+sudo add-apt-repository multiverse
+sudo apt-add-repository ppa:ubuntudde-dev/stable
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo apt update
+sudo apt install steam wine64 wine32 -y
+sudo apt install ubuntudde-dde kde-plasma-desktop gnome-session gdm3 -y
